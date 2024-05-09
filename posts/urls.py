54267d7cd0f1fe1707ee7020cdeb3659482ce64f
Pablo Escobar <Guillermo.escobar11@yahoo.com>
2024-05-08 20:05:47 -0700

    new

--- posts/urls.py
+++ posts/urls.py
@@ -4,8 +4,9 @@ from posts import views
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="list"),
+    path("drafts/", views.DraftPostListView.as_view(), name="drafts"),
     path("<int:pk>/", views.PostDetailView.as_view(), name="detail"),
     path("new/", views.PostCreateView.as_view(), name="new"),
-    path("edit/<int:pk>/", views.PostUpdateView.as_view(), name="edit"),
-    path("delete/<int:pk>/", views.PostDeleteView.as_view(), name="delete"),
+    path("<int:pk>/edit/", views.PostUpdateView.as_view(), name="edit"),
+    path("<int:pk>/delete/", views.PostDeleteView.as_view(), name="delete"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,3 @@
-from django.forms import BaseModelForm
-from django.http import HttpResponse
 from django.views.generic import (
     ListView, 
     DetailView, 
@@ -8,12 +6,26 @@ from django.views.generic import (
     UpdateView
 )
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post, Status
 
 class PostListView(ListView):
     template_name = "post/list.html"
     model = Post
+    # make this one visible to show only puplished post to everyone
+
+class DraftPostListView(LoginRequiredMixin, ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        draft_status = Status.objects.get(name="draft")
+        context["post_list"] = Post.objects.filter(
+            status=draft_status).filter(
+                author=self.request.user).order_by("created_on").reverse()
+        return context
+       
 
 class PostDetailView(DetailView):
     template_name = "post/detail.html"
@@ -22,18 +34,26 @@ class PostDetailView(DetailView):
 class PostCreateView(LoginRequiredMixin, CreateView):
     template_name = "post/new.html"
     model = Post
-    fields = ["title", "subtitle", "author", "body", "status"]
+    fields = ["title", "subtitle", "body", "status"]
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = "post/edit.html"
     model = Post
     fields = ["title", "subtitle", "body", "status"]
 
-class PostDeleteView(DeleteView):
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     template_name = "post/delete.html"
     model = Post
     success_url = reverse_lazy("list")
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user

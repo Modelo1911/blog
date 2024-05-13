0c2206a31c038f4811b7682d1d90421a6f894a6a
Pablo Escobar <Guillermo.escobar11@yahoo.com>
2024-05-13 15:58:08 -0700

    new4

--- config/urls.py
+++ config/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path("", include("pages.urls")),
     path("posts/", include("posts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("accounts/", include("accounts.urls")),
 ]
+
+## path("accounts/", include("accounts.urls")),
